252a02233573372be8638434cf9d614b90aaadf2
NeonChrome93 <daarksky1919@gmail.com>
2024-03-10 23:58:37 +0300

    transfer adapters

---
+++ src/features/auth/adapters/jwt.adapter.ts
@@ -0,0 +1,51 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../../users/domain/db-model';
+
+@Injectable()
+export class JwtAdapter {
+    // constructor(private readonly jwtService: JwtService)
+
+    createJWT(user: User) {
+        //TODO: 10s
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET || '123', {
+            expiresIn: process.env.ACCESS_TIME,
+        });
+        return token;
+    }
+
+    getUserIdByToken(token: string) {
+        try {
+            const result: any = jwt.verify(token, process.env.JWT_SECRET || '123');
+            return result.userId;
+        } catch (error) {
+            return null;
+        }
+    }
+
+    //jwt.decode - можно достать дату выдачи и сохранить в БД + добавить переменную девайс ID
+    generateRefreshToken(user: User, deviceId: string) {
+        //deviceId
+        return jwt.sign({ userId: user.id, deviceId: deviceId }, process.env.JWT_SECRET || '123', {
+            expiresIn: process.env.REFRESH_TIME,
+        });
+    }
+
+    getPayloadByToken(token: string) {
+        try {
+            const result: any = jwt.verify(token, process.env.JWT_SECRET || '123');
+            return result; //\==={userId: user._id, deviceId: deviceId}
+        } catch (error) {
+            return null;
+        }
+    }
+
+    lastActiveDate(token: string): string {
+        const result: any = jwt.decode(token);
+        return new Date(result.iat * 1000).toISOString(); //милесекунды и в строку
+        //дата выписки токена это мое последнее посещение, закинуть в девайс репу
+    }
+
+    //создать токен с настройками и вернуть токен в куку createCookieToken +
+}
