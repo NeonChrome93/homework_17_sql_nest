51c37f22fb340ddda0bb48dfb71abbc7df2c0206
NeonChrome93 <daarksky1919@gmail.com>
2024-03-15 23:59:07 +0300

    added testing/all-data logic

---
+++ src/testing-all-data/testing.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Delete, HttpCode } from '@nestjs/common';
+import { UserRepository } from '../features/users/repositories/user-repository';
+import { DataSource } from 'typeorm';
+
+@Controller('testing')
+export class DelController {
+    constructor(private dataSource: DataSource) {}
+    @Delete('all-data')
+    @HttpCode(204)
+    async DeleteAllData() {
+        await this.dataSource.query(`DELETE FROM public.devices CASCADE`);
+        await this.dataSource.query(`DELETE FROM public.users CASCADE`);
+    }
+}
