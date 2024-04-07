1c69b025113e00b07f9e4cb9b6e46928c759e5a9
NeonChrome93 <daarksky1919@gmail.com>
2024-04-07 23:59:15 +0300

    trying added json_build_object queries for comments

--- src/features/public/comments/repositories/comment.query.repository.ts
+++ src/features/public/comments/repositories/comment.query.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { QueryPaginationType } from '../../../../utils/pagination';
 import { DataSource } from 'typeorm';
 import { CommentsViewType } from '../api/models/output/comments.output.models';
+import { query } from 'express';
 
 @Injectable()
 export class CommentsQueryRepository {
@@ -42,27 +43,37 @@ export class CommentsQueryRepository {
         return postId;
     }
 
-    async readCommentId(id: string, userId?: string | null): Promise</*<CommentsViewType*/ null> {
-        //const comment: CommentsDBType | null = await this.CommentModel.findOne({ _id: new ObjectId(id) });
+    async readCommentId(id: string, userId?: string | null): Promise<CommentsViewType | null> {
+        const query = `WITH comment_info AS (
+    SELECT 
+        c.id AS comment_id,
+        c.content AS content,
+        c.userId AS commentator_id,
+        a.userLogin AS user_login,
+        c.createdAt AS created_at
+    FROM comments c
+    JOIN accounts a ON c.userId = a.id
+    ),
+    likes_info AS (
+    SELECT 
+        c.id AS comment_id,
+        COUNT(CASE WHEN l.status = 'Like' THEN 1 END) AS likes_count,
+        COUNT(CASE WHEN l.status = 'Dislike' THEN 1 END) AS dislikes_count,
+        SUM(CASE WHEN l.userId = 'myUserId' THEN 1 ELSE 0 END) AS my_status
+    FROM comments c
+    LEFT JOIN likes l ON c.id = l.commentId
+    GROUP BY c.id
+    )
+    SELECT 
+        c.comment_id AS id,
+        c.content AS content,
+        json_build_object('userId', c.commentator_id, 'userLogin', c.user_login) AS commentatorInfo,
+        c.created_at AS createdAt,
+        json_build_object('likesCount', COALESCE(l.likes_count, 0), 'dislikesCount', COALESCE(l.dislikes_count, 0), 'myStatus', CASE WHEN l.my_status > 0 THEN 'Like' ELSE 'None' END) AS likesInfo
+        FROM comment_info c
+        LEFT JOIN likes_info l ON c.comment_id = l.comment_id;`;
 
-        // if (!comment) {
-        //     return null;
-        // }
-        // return {
-        //     id: comment._id.toString(),
-        //     content: comment.content,
-        //     commentatorInfo: comment.commentatorInfo,
-        //     createdAt: comment.createdAt.toISOString(),
-        //     likesInfo: {
-        //         likesCount: comment.reactions.filter(r => r.status === REACTIONS_ENUM.Like).length,
-        //         dislikesCount: comment.reactions.filter(r => r.status === REACTIONS_ENUM.Dislike).length,
-        //         myStatus: userId
-        //             ? comment.reactions.filter(r => r.userId === userId).length
-        //                 ? comment.reactions.filter(r => r.userId === userId)[0].status
-        //                 : REACTIONS_ENUM.None
-        //             : REACTIONS_ENUM.None,
-        //     },
-        // };
-        return null;
+        const view = await this.dataSource.query(query);
+        return view[0];
     }
 }
